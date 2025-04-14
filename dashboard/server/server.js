e982eb49c87a5e504b30ea3bc7b6dc34cd586e85
Burtarn <robin.andersson0125@gmail.com>
2025-04-14 16:01:12 +0200

    Updaterade mappstruktur i backend

--- dashboard/server/server.js
+++ dashboard/server/server.js
@@ -23,40 +23,12 @@ app.use(session({
     }
 }));
 
-const users = [
-    { username: 'admin', password: '1234' }
-];
+const authRoutes = require('./routes/AuthRoutes');
+app.use(authRoutes);
 
-app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-    const user = users.find(u => u.username === username && u.password === password);
-    if (user) {
-        req.session.user = user;
-        res.status(200).json({ message: 'Inloggning lyckades' });
-    } else {
-        res.status(401).json({ message: 'Fel användarnamn eller lösenord' });
-    }
-});
 
-app.get('/check-auth', (req, res) => {
-    if (req.session.user) {
-        res.json({ authenticated: true, user: req.session.user });
-    } else {
-        res.json({ authenticated: false });
-    }
-});
-
-app.post('/logout', (req, res) => {
-    req.session.destroy(err => {
-        if (err) {
-            return res.status(500).json({ message: 'Kunde inte logga ut' });
-        }
-        res.clearCookie('connect.sid');
-        res.json({ message: 'Utloggad' });
-    });
-});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servern körs på port ${PORT}`);
+    console.log(`Servern körs på port ${PORT}`);
 });
